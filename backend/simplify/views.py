3b55310955764a79a21a5704fba28da8e4b46836
davidcheungo123 <65503074+davidcheungo123@users.noreply.github.com>
2022-04-14 15:07:00 +0800

    updated backend tokenizer

--- backend/simplify/views.py
+++ backend/simplify/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework.response import Response
 import trafilatura
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer
 
 class SimplifyTextView(APIView):
   permission_classes = [AllowAny]
@@ -19,26 +19,45 @@ class SimplifyTextView(APIView):
       return Response({"error": "text/url both found"}, status=status.HTTP_400_BAD_REQUEST)
     # elif not text:
     else:
-      # print("------------------------------------------------1")
+      print("------------------------------------------------1")
       pipe = pipeline("summarization", model="google/pegasus-xsum")
-      # print("------------------------------------------------2")
+      model_ckpt = "distilbert-base-uncased"
+      print("------------------------------------------------2")
+      tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+      print("------------------------------------------------3")
       if not text:
+        print()
         downloaded = trafilatura.fetch_url(url)
         main_text = trafilatura.extract(downloaded, include_comments=False, include_tables=False)
-        simplified_text = pipe(main_text, max_length=512, clean_up_tokenization_spaces=True)
+        encoded_text = tokenizer(main_text)
+        tokens = tokenizer.convert_ids_to_tokens(encoded_text.input_ids)
+        if len(tokens) > 512:
+          tokens = tokens[:450]
+        main_text_cleared = tokenizer.convert_tokens_to_string(tokens)
+        simplified_text = pipe(main_text_cleared[5:], max_length=512, clean_up_tokenization_spaces=True)
         # TODO: run model with main_text as input
         # simplified_text = ''
         return Response({
           'textContent': main_text, 
-          'simplifiedText': simplified_text
+          'simplifiedText': simplified_text[0]["summary_text"]
           }, status=status.HTTP_200_OK)
       else:
-        # print("------------------------------------------------3")
-        simplified_text = pipe(text, max_length=512, clean_up_tokenization_spaces=True)
-        # print("------------------------------------------------4")
+        print("------------------------------------------------4")
+        encoded_text = tokenizer(text)
+        print("------------------------------------------------5")
+        tokens = tokenizer.convert_ids_to_tokens(encoded_text.input_ids)
+        print("------------------------------------------------6")
+        if len(tokens) > 512:
+          print("------------------------------------------------7")
+          tokens = tokens[:450]
+        print("------------------------------------------------8")
+        text_cleared = tokenizer.convert_tokens_to_string(tokens)
+        print("------------------------------------------------9")
+        simplified_text = pipe(text_cleared[5:], max_length=512, clean_up_tokenization_spaces=True)
+        print("------------------------------------------------10")
         # TODO: run model with text as input
         # simplified_text = model(text)
         return Response({
           'textContent': text,
-          'simplifiedText': simplified_text
+          'simplifiedText': simplified_text[0]["summary_text"]
           }, status=status.HTTP_200_OK)
